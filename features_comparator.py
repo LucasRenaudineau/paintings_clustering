fd72c00546fdfdf708520070fd6d916b168ca440
Lucas Renaudineau <lucas.renaudineau@telecom-paris.fr>
2026-05-20 11:31:46 +0200

    comparing features of a lot of images

--- features_comparator.py
+++ features_comparator.py
@@ -63,20 +63,38 @@ def compute_distances_one_to_many(image_path,comparedImagesPaths, feature_model)
     activation_set = extract_features(image, feature_model)
     compared_activations_sets = [extract_features(im, feature_model) for im in comparedImages]
 
+    distances = []
+
     for i in range(len(comparedImagesPaths)):
         print(f"Distance between {image_path} and {comparedImagesPaths[i]} is:")
-        print(distance(activation_set, compared_activations_sets[i]))
+        dist = distance(activation_set, compared_activations_sets[i])
+        print(dist)
+        distances.append(dist)
+    return distances
 
 # Find paths of n images
 def find_paths(n:int):
     pass
 
-# Save the nearest and furthest images from the dataset
-def save_nearest_and_furthest_images(image_path, comparedImagesPaths, n):
-    pass
+# Save the n nearest and n furthest images from the dataset
+def save_nearest_and_furthest_images(image_path, comparedImagesPaths, n, feature_model):
+    distances = compute_distances_one_to_many(image_path, comparedImagesPaths, feature_model)
+    np_distances = np.array(distances)
+    min_indices = np.argpartition(np_distances, n-1)[:n]
+    sorted_min_indices = min_indices[np.argsort(np_distances[min_indices])]
+    max_indices = np.argpartition(np_distances, -n)[-n:]
+    sorted_max_indices = max_indices[np.argsort(np_distances[max_indices])[::-1]]
+
+    for i in range(n):
+        min_index = sorted_min_indices[i]
+        max_index = sorted_max_indices[i]
+        cv2.imwrite("outputs/n_min/"+comparedImagesPaths[min_index].split("/")[-1])
+        print(f"In n_min : {comparedImagesPaths[min_index]} with a score of {distances[min_index]}.")
+        cv2.imwrite("outputs/n_max/"+comparedImagesPaths[max_index].split("/")[-1])
+        print(f"In n_min : {comparedImagesPaths[max_index]} with a score of {distances[max_index]}.")
 
 if __name__ =="__main__":
     _, feature_model = load_model()
     image_path = "ArtemisArt/braque - georges-braque_1882/braque_1.jpg"
     comparedImagesPaths = ["ArtemisArt/braque - georges-braque_1882/braque_1.jpg", "ArtemisArt/braque - georges-braque_1882/braque_2.jpg", "ArtemisArt/braque - georges-braque_1882/braque_3.jpg", "ArtemisArt/braque - georges-braque_1882/braque_4.jpg", "ArtemisArt/braque - georges-braque_1882/braque_5.jpg", "ArtemisArt/braque - georges-braque_1882/braque_6.jpg", "ArtemisArt/braque - georges-braque_1882/braque_7.jpg", "ArtemisArt/brauner - victor-brauner_1903/brauner_1.jpg", "ArtemisArt/bronzino - agnolo-di-cosimi-dit-bronzino_1503/bronzino_1.jpg", "ArtemisArt/brueghel-j - jan-brueghel-l-ancien-dit-de-velours_1568/brueghel-j-I_1.jpg", "ArtemisArt/delacroix - eugene-delacroix_1798/delacroix_33.jpg"]
-    compute_distances_one_to_many(image_path, comparedImagesPaths, feature_model)
+    _ = compute_distances_one_to_many(image_path, comparedImagesPaths, feature_model)
