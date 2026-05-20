17ca9af262cec34660004c9b525144141daae7a4
Lucas Renaudineau <lucas.renaudineau@telecom-paris.fr>
2026-05-20 10:13:47 +0200

    structure of comparator

---
+++ comparator.py
@@ -0,0 +1,10 @@
+# Distance function between 2 activations
+def distance(activations1, activations2):
+    pass
+
+# Test function to compute distances between an image and a list of images
+def compute_distances_one_to_many(image,images_list):
+    pass
+
+if __name__ =="__main__":
+    pass
